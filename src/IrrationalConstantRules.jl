472d683b7df249ca70a37dbe1ab40710e043b7f3
hyrodium <hyrodium@gmail.com>
2022-04-29 01:47:49 +0900

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/IrrationalConstantRules.jl
@@ -0,0 +1,5 @@
+module IrrationalConstantRules
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using IrrationalConstantRules
+using Test
+
+@testset "IrrationalConstantRules.jl" begin
+    # Write your tests here.
+end
